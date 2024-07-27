f9f254f1a2400d7359b336a58cced653a58d2426
MitchellMaliglig <mitchell.maliglig@gmail.com>
2024-07-27 03:18:07 +0000

    Add an 'input' event listener to the Photo URL input that sets the src attribute of the photo preview to the input's value when the user types or pastes in a new URL. #1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
+'use strict';
 /* global data */
-
-function checkUrl(str: string): boolean {
-  let url: URL;
-
+function checkUrl(str) {
+  let url;
   try {
     url = new URL(str);
   } catch {
     return false;
   }
-
   return url.protocol === 'http:' || url.protocol === 'https:';
 }
-
-const $image = document.querySelector('img') as HTMLImageElement;
+let $image = document.querySelector('img');
 if (!$image) throw new Error('$image missing');
 console.log('$image: ', $image);
-
-const $photoInput = document.querySelector(
-  'input#photo-url-input',
-) as HTMLInputElement;
+let $photoInput = document.querySelector('input#photo-url-input');
 if (!$photoInput) throw new Error('$photoInput missing');
 console.log('$photoInput: ', $photoInput);
-
 /*
 let badUrl = '
   haha nope
@@ -30,17 +23,11 @@ let goodUrl = '
   https://www.budgetbytes.com/wp-content/uploads/2020/06/BBQ-Chicken-Pizza-V2.jpg
 ';
 */
-
-$photoInput.addEventListener('input', function (event: Event) {
-  const $eventTarget = event.target as HTMLInputElement;
-
+$photoInput.addEventListener('input', function (event) {
+  let $eventTarget = event.target;
   if (checkUrl($eventTarget.value)) {
     $image.src = $eventTarget.value;
   } else {
     $image.src = 'images/placeholder-image-square.jpg';
   }
 });
-
-// 1:11  error  'data' is defined but never used  @typescript-eslint/no-unused-vars
-const false_ = false;
-if (false_) console.log(data);
